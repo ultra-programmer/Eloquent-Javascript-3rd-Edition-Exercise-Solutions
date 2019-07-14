99866c363059caee15377684f11b66603b38a47b
ultraprogrammer <ultraprogrammer13@gmail.com>
2019-07-14 09:38:21 -0400

    Create Count-Chars.js

---
+++ Chapter-3/Count-Chars.js
@@ -0,0 +1,4 @@
+// A function that returns the number of occurences a given character has in a given string
+function countChar(str, char) {
+  return str.split('').filter(c => c === char).length;
+}
