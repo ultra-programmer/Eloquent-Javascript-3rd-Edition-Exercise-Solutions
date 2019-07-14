e6fadd985483342acbbe9f7cb6841a26e9a6d3ca
ultraprogrammer <ultraprogrammer13@gmail.com>
2019-07-14 08:57:03 -0400

    Create Looping-Triangle.js

---
+++ Chapter-2/Looping-Triangle.js
@@ -0,0 +1,12 @@
+/* A basic program that prints out the following in the JavaScript console:
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+*/
+for (let i = 1; i <= 7; i++) {
+  console.log("#".repeat(i));
+}
