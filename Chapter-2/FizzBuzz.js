11f3fdaaea39fd83959ab6bef041b0b546d07181
ultraprogrammer <ultraprogrammer13@gmail.com>
2019-07-29 16:25:48 -0400

    Removed unnecessary brackets.

--- Chapter-2/FizzBuzz.js
+++ Chapter-2/FizzBuzz.js
@@ -1,4 +1,2 @@
 // My overcomplicated FizzBuzz Solution
-for (num of [...Array(100).keys()].slice(1)) {
-  console.log( ( (num % 3 === 0 ? "Fizz" : "") + (num % 5 === 0 ? "Buzz" : "") ) || num)
-}
+for (num of [...Array(100).keys()].slice(1)) console.log( ( (num % 3 === 0 ? "Fizz" : "") + (num % 5 === 0 ? "Buzz" : "") ) || num);
