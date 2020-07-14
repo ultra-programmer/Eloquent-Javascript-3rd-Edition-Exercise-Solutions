fbb6a2538f0fd8fd51c461bdae8d211e9d47afbd
ultraprogrammer <ultraprogrammer13@gmail.com>
2020-07-14 17:01:06 -0400

    Updated FizzBuzz.js
    Made sure that the file printed all numbers up AND INCLUDING 100.

--- Chapter-2/FizzBuzz.js
+++ Chapter-2/FizzBuzz.js
@@ -1,2 +1,2 @@
 // My overcomplicated FizzBuzz Solution
-for (num of [...Array(100).keys()].slice(1)) console.log( ( (num % 3 === 0 ? "Fizz" : "") + (num % 5 === 0 ? "Buzz" : "") ) || num);
+for (num of [...Array(101).keys()].slice(1)) console.log( ( (num % 3 === 0 ? "Fizz" : "") + (num % 5 === 0 ? "Buzz" : "") ) || num);
